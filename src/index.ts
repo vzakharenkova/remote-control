5f6a482f2c5891d4d37b754480f8489ef9d99661
vzakharenkova <v.s.zakharenkova@gmail.com>
2023-01-16 19:13:24 +0300

    feat: create bebsocket server

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,40 @@
 import { httpServer } from './http_server/createServer.js';
-import { mouse } from '@nut-tree/nut-js';
+import { WebSocketServer, createWebSocketStream } from 'ws';
+import { HANDLERS } from './backend/utils/shared.js';
 
-const HTTP_PORT = 8182;
+const HTTP_PORT = 8080;
 
-console.log(`Start static http server on the ${HTTP_PORT} port!`);
-httpServer.listen(HTTP_PORT);
+const wsServer = new WebSocketServer({ server: httpServer });
+
+wsServer.on('connection', async (ws) => {
+  console.log('WebSocket connnection is started!');
+
+  const duplex = createWebSocketStream(ws, { encoding: 'utf8', decodeStrings: false });
+
+  duplex.on('data', async (data) => {
+    console.log('received: %s', data);
+
+    const [command, ...args]: [string, ...string[]] = data.split(' ');
+
+    HANDLERS[command](command, args, duplex);
+  });
+
+  duplex.on('close', () => console.log('WebSocket connnection is closed!'));
+});
+
+wsServer.on('close', () => console.log('WebSocket Server is closed!'));
+
+httpServer.on('close', () => {
+  wsServer.clients.forEach((client) => client.terminate());
+  console.log('HTTP server is closed!');
+});
+
+process.on('SIGINT', () => {
+  wsServer.clients.forEach((client) => client.terminate());
+  httpServer.close();
+  process.exit();
+});
+
+httpServer.listen(HTTP_PORT, () => {
+  console.log(`Start static http server on the ${HTTP_PORT} port!`);
+});
